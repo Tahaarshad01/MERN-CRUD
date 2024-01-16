c9b6288e9f0a06b957899c20965345bab5a96a87
Taha_Arshad <ta.arshad786@gmail.com>
2024-01-17 00:52:04 +0530

    redirects

--- server/index.js
+++ server/index.js
@@ -17,12 +17,15 @@ require("./db/conn.js");
 const Register = require("./model/regis.js");
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", ["OPTIONS","GET, POST, PUT, DELETE"]);
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
-});
+}); 
 
 //insert api///
 app.post("/register", async (req, res) => {
